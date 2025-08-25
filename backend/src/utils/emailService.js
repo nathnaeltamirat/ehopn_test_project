ccdeb1d014c85dd6105dd7b65c66a09e0be65d9c
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-08-25 00:19:21 -0900

    fixed

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -6,7 +6,7 @@ const createTransporter = () => {
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORDWORD
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 };
